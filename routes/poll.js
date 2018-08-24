b0dbe8fe12bae1b015c6063849ba0a63ee47f5fe
diaaposto <dia.apostolakis@icloud.com>
2018-08-24 22:49:40 +0000

    changes to poll ejs and js file and added an ajax call to app js

--- routes/poll.js
+++ routes/poll.js
@@ -5,9 +5,15 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
+  router.post("/:url", (req, res) => {
+    console.log(req.body);
+    //now insert into options table
+
+  })
+
   router.get("/:url", (req, res) => {
     knex
-      .select("poll.question", "option.name")
+      .select("poll.question", "option.name", "poll_id")
       .from("poll")
       .join("option", "poll.id", "=", "option.poll_id")
       .where(knex.raw("voting_url = ?", req.params.url))
@@ -17,7 +23,9 @@ module.exports = (knex) => {
         } else {
           let templateVars = {
             question: results[0].question,
-            options: []
+            options: [],
+            url: req.params.url,
+            poll_id: results[0].poll_id
            }
 
           for (let entry of results) {
