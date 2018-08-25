533f886a957712952a1f652bc1c0231793c703d6
diaaposto <dia.apostolakis@icloud.com>
2018-08-25 01:55:35 +0000

    accepted changes for appjs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,20 +1,29 @@
 $(document).ready(() => {
-    $(".poll").on("submit", function(event) {
-        event.preventDefault();
-        const optionsArr = [];
-        const elements = document.querySelectorAll(".option-element");
-        for (var i = 0; i < elements.length; i++) {
-          optionsArr.push(elements[i].innerText);
-        }
-        // console.log(options);
-        const pollId = $("input[name='poll_id']").val();
-        const url = $("input[name='url']").val();
-        const data = { pollId: pollId, options: optionsArr };
-        console.log(data);
-        $.ajax( {url:'/poll/' + url, data: data, method:'post' } ).done(function(){
-
-        }).catch(function(err){
-            console.log(err);
-        })
+  $(".poll").on("submit", function(event) {
+    event.preventDefault();
+    const optionsArr = [];
+    const elements = document.querySelectorAll(".option-element");
+    for (var i = 0; i < elements.length; i++) {
+      optionsArr.push({
+        name: elements[i].innerText,
+        id: elements[i].getAttribute("optionId")
+      });
+    }
+    // console.log(options);
+    const pollId = $("input[name='poll_id']").val();
+    const url = $("input[name='url']").val();
+    const data = { 
+      pollId: pollId, 
+      options: optionsArr 
+    };
+    $.ajax( {
+      url:'/poll/' + url, 
+      data: data, 
+      method:'post' 
+    }).done(function(){
+  
+    }).catch(function(err){
+      console.log(err);
     })
+  })
 })
